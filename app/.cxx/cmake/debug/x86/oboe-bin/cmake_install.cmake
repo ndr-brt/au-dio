c9996014b857da0d4f3478811239a532ad876cd0
Andrea Bertagnolli <andrea.bertagnolli@cherrychain.it>
2020-02-17 22:25:00 +0100

    Fix package

--- app/.cxx/cmake/debug/x86/oboe-bin/cmake_install.cmake
+++ app/.cxx/cmake/debug/x86/oboe-bin/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/andrea/Code/personal/dio-au/oboe
+# Install script for directory: /home/andrea/Code/personal/au-dio/oboe
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -38,10 +38,10 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
 endif()
 
 if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/x86" TYPE STATIC_LIBRARY FILES "/home/andrea/Code/personal/dio-au/app/.cxx/cmake/debug/x86/oboe-bin/liboboe.a")
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/x86" TYPE STATIC_LIBRARY FILES "/home/andrea/Code/personal/au-dio/app/.cxx/cmake/debug/x86/oboe-bin/liboboe.a")
 endif()
 
 if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include" TYPE DIRECTORY FILES "/home/andrea/Code/personal/dio-au/oboe/include/oboe")
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include" TYPE DIRECTORY FILES "/home/andrea/Code/personal/au-dio/oboe/include/oboe")
 endif()
 
