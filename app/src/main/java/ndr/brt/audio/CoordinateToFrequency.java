a8391d62fdd82728ec58005cf19111617d128d68
Andrea Bertagnolli <andrea.bertagnolli@cherrychain.it>
2020-02-18 21:57:08 +0100

    Remove comments

--- app/src/main/java/ndr/brt/audio/CoordinateToFrequency.java
+++ app/src/main/java/ndr/brt/audio/CoordinateToFrequency.java
@@ -12,13 +12,6 @@ public interface CoordinateToFrequency extends BiFunction<Float, Integer, Float>
     float MAXIMUM_FREQUENCY = 10000f;
 
     static CoordinateToFrequency coordinateToFrequency() {
-        return (coordinate, total) -> {
-            return MAXIMUM_FREQUENCY - (float) ((MAXIMUM_FREQUENCY * Math.log10(coordinate + 1))/Math.log10(total + 1));
-        };
+        return (coordinate, total) -> MAXIMUM_FREQUENCY - (float) ((MAXIMUM_FREQUENCY * Math.log10(coordinate + 1))/Math.log10(total + 1));
     }
-    // 20000 = k log total + c ; 0 = k log 1 + c
-    // c = 0
-    // k = 20000 / log total
-    // f(n) = 20000 / log total
-    // f(x) = 20000*log(x+1)/log(total+1)
 }
